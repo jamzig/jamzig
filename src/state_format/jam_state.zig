1329014424e477b68f3ef945d1db2a85a864e668
Boy Maas <boy.maas@gmail.com>
2024-12-15 15:45:07 +0100

    feat: jam_params

--- src/state_format/jam_state.zig
+++ src/state_format/jam_state.zig
@@ -2,49 +2,7 @@ const std = @import("std");
 const JamState = @import("../state.zig").JamState;
 const Params = @import("../jam_params.zig").Params;
 
-fn IndentWriter(comptime Writer: type) type {
-    return struct {
-        wrapped: Writer,
-        indent: []const u8,
-        at_start: bool,
-
-        const Self = @This();
-        pub const Error = anyerror;
-
-        pub fn init(wrapped: Writer, indent: []const u8) Self {
-            return .{
-                .wrapped = wrapped,
-                .indent = indent,
-                .at_start = true,
-            };
-        }
-
-        pub fn write(self: *Self, bytes: []const u8) Error!usize {
-            var written: usize = 0;
-
-            for (bytes) |byte| {
-                if (self.at_start) {
-                    written += try self.wrapped.write(self.indent);
-                    self.at_start = false;
-                }
-                written += try self.wrapped.write(&[_]u8{byte});
-                // Only set at_start to true when we actually see a newline
-                if (byte == '\n') {
-                    self.at_start = true;
-                }
-            }
-
-            // We need to return the number of bytes written as expected
-            // by the caller. This is not the actual number of bytes written but an indicator
-            // that we finished writing. This happens when we have written all bytes.
-            return bytes.len;
-        }
-
-        pub fn writer(self: *Self) std.io.Writer(*Self, Error, write) {
-            return .{ .context = self };
-        }
-    };
-}
+const tfmt = @import("../types/fmt.zig");
 
 pub fn format(
     comptime P: Params,
@@ -55,16 +13,29 @@ pub fn format(
 ) !void {
     _ = fmt;
     _ = options;
-    try writer.writeAll("JamState{\n");
-    // Then format each component
+
+    var indented_writer = tfmt.IndentedWriter(@TypeOf(writer)).init(writer);
+    const iw = indented_writer.writer();
+
+    try iw.print(
+        "JamState(validators_count={d},core_count={d}):\n",
+        .{ P.validators_count, P.core_count },
+    );
+    iw.context.indent();
+
     inline for (std.meta.fields(@TypeOf(self.*))) |field| {
-        try writer.print(" \x1b[1;37m{s}\x1b[0m:\n", .{field.name});
-        // Create indented writer and ensure first line is indented
-        var indent_writer = IndentWriter(@TypeOf(writer)).init(writer, "   ");
-        // Force the first line to be indented
-        indent_writer.at_start = true;
-        try std.fmt.format(indent_writer.writer(), "{any}", .{@field(self, field.name)});
-        try writer.writeByte('\n');
+        try iw.print("{s}: ", .{field.name});
+        try tfmt.formatValue(@field(self.*, field.name), iw);
     }
-    try writer.writeAll("}");
+}
+
+test "JamStateFormat" {
+    const allocator = std.testing.allocator;
+    const TINY = @import("../jam_params.zig").TINY_PARAMS;
+
+    var state = try JamState(TINY).init(allocator);
+    defer state.deinit(allocator);
+
+    // Print the JSON string (you can comment this out if you don't want to print)
+    std.debug.print("FORMAT: \n\n{any}\n", .{state});
 }
--- src/tests.zig
+++ src/tests.zig
@@ -48,7 +48,8 @@ comptime {
     _ = @import("state_format/recent_blocks.zig"); // TODO: rename to beta
     _ = @import("state_format/delta.zig");
     _ = @import("state_format/psi.zig"); // TODO: rename to psi
-    _ = @import("state_format/jam_params.zig"); // TODO: rename to psi
+    _ = @import("state_format/jam_params.zig");
+    _ = @import("state_format/jam_state.zig");
 
     _ = @import("services.zig");
     _ = @import("services_priviledged.zig");
