a34616098f247ebdb1f4beed5e20acee29b64ee4
Boy Maas <boy.maas@gmail.com>
2025-07-16 15:25:06 +0200

    refactor: crypto lib

--- src/crypto.zig
+++ src/crypto.zig
@@ -1,7 +1,9 @@
-// TODO: rename to bandersnatch
+//! Cryptographic primitives for JAM implementation.
+//! Provides unified access to various elliptic curve operations and signature schemes.
 
 const std = @import("std");
 const types = @import("types.zig");
 
+// External crypto modules
 pub const bandersnatch = @import("crypto/bandersnatch.zig");
 pub const bls12_381 = @import("crypto/bls12_381.zig");
